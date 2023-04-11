7b347ac5fa1492a9f9dc57475da3a07a293438f8
Benediximuss <ugurpasa699@gmail.com>
2023-04-12 02:36:35 +0300

    cs201 commit

--- CS201-Intro-to-Computing/Homework 5 - Classes/Reservation.h
+++ CS201-Intro-to-Computing/Homework 5 - Classes/Reservation.h
@@ -8,10 +8,43 @@ class Sailor
 	public:
 		Sailor()
 		{
-			id = 0;
-			name = "Jane Doe";
+			id = 999;
+			name = "DEFAULT";
 			age = 0;
 			rank = 0;
+		}
+
+		Sailor(int a, string b, double c, double d)
+		{
+			id = a;
+			name = b;
+			age = c;
+			rank = d;
+		}
+
+		int getSID()
+		{
+			return id;
+		}
+
+		string getSailorName()
+		{
+			return name;
+		}
+
+		double getAge()
+		{
+			return age;
+		}
+
+		double getRank()
+		{
+			return rank;
+		}
+
+		void updateSID(int a)
+		{
+			id = a;
 		}
 
 	private:
@@ -26,9 +59,36 @@ class Boat
 	public:
 		Boat()
 		{
-			id = 0;
-			name = "Default";
-			color = "Default";
+			id = 999;
+			name = "DEFAULT";
+			color = "DEFAULT";
+		}
+
+		Boat(int a, string b, string c)
+		{
+			id = a;
+			name = b;
+			color = c;
+		}
+
+		int getBID()
+		{
+			return id;
+		}
+
+		string getBoatName()
+		{
+			return name;
+		}
+
+		string getColor()
+		{
+			return color;
+		}
+
+		void updateBID(int a)
+		{
+			id = a;
 		}
 
 	private:
@@ -50,7 +110,32 @@ class Reservation
 			resDate = d;
 		}
 
-		// Add here the Print function
+		Reservation(Date a, Sailor b, Boat c)
+		{
+			resDate = a;
+			sailor = b;
+			boat = c;
+		}
+
+		Date getDate()
+		{
+			return resDate;
+		}
+
+		Sailor getSailorInfo()
+		{
+			return sailor;
+		}
+
+		Boat getBoatInfo()
+		{
+			return boat;
+		}
+
+		void printReservation()
+		{
+			cout << resDate << " -> " << sailor.getSailorName() << "(" << sailor.getRank() << "," << sailor.getAge() << ") has reserved " << boat.getBoatName() << "(" << boat.getColor() << ")" << endl; 
+		}
 
 	private:
 		Date resDate;
@@ -61,14 +146,199 @@ class Reservation
 class Reservations
 {
 	public:
-		// Fill this part with the member functions
-		// 1. Add a Reservation
 
-		// 2. Delete a Reservation
+		void addRes(Reservation a)
+		{
+			reservations.push_back(a);
+		}
 
-		// 3.1. Find a Reservation (sailor and boat ids are 0)
-		// 3.2. Find a Reservation (only sailor id is 0)
-		// 3.3. Find a Reservation (only boat id is 0)
+		void delRes(Reservation a, string b)
+		{
+			if (a.getSailorInfo().getSID() == 0) {
+
+				int len = reservations.size();
+				int target;
+				int delTimes = 0;
+
+				for (int i = 0; i < len; i++) {
+					if (reservations[i].getBoatInfo().getBID() == a.getBoatInfo().getBID()) {
+						delTimes++;
+					}
+				}
+
+				int k = 0;
+				while (k != delTimes) {
+					len = reservations.size();
+					int last_idx = len - 1;
+					for (int i = 0; i < len; i++){
+						if (reservations[i].getBoatInfo().getBID() == a.getBoatInfo().getBID()) {
+							target = i;
+							reservations[target] = reservations[last_idx];
+							reservations.pop_back();
+							break;
+						}
+					}
+					k++;
+				}
+
+				if (delTimes == 0) {
+					cout << "Error: Could not delete reservation for boat id " << a.getBoatInfo().getBID() << endl << endl;
+				}		
+			
+			}
+			
+			else if (a.getBoatInfo().getBID() == 0) {
+
+				int len = reservations.size();
+				int target;
+				int delTimes = 0;
+
+				for (int i = 0; i < len; i++) {
+					if (reservations[i].getSailorInfo().getSID() == a.getSailorInfo().getSID()) {
+						delTimes++;
+					}
+				}
+
+				int k = 0;
+				while (k != delTimes) {
+					len = reservations.size();
+					int last_idx = len - 1;
+					for (int i = 0; i < len; i++){
+						if (reservations[i].getSailorInfo().getSID() == a.getSailorInfo().getSID()) {
+							target = i;
+							reservations[target] = reservations[last_idx];
+							reservations.pop_back();
+							break;
+						}
+					}
+					k++;
+				}
+
+				if (delTimes == 0) {
+					cout << "Error: Could not delete reservation for sailor id " << a.getSailorInfo().getSID() << endl << endl;
+				}						
+		
+			}
+
+			else if (a.getSailorInfo().getSID() != 0 && a.getBoatInfo().getBID() != 0) {
+
+				int len = reservations.size();
+				int target;
+				bool check = false;
+
+				for (int i = 0; i < len; i++ ) {
+					if (reservations[i].getSailorInfo().getSID() == a.getSailorInfo().getSID() && reservations[i].getBoatInfo().getBID() == a.getBoatInfo().getBID() && reservations[i].getDate() == a.getDate()) {
+						target = i;
+						check = true;
+						break;
+					}
+				}
+
+				if (check == true) {
+					int last_idx = len - 1;
+					reservations[target] = reservations[last_idx];
+					reservations.pop_back();
+				}
+
+				else {
+					cout << "Error: Could not delete reservation " << "\"" << a.getSailorInfo().getSID() << " " << a.getBoatInfo().getBID() << " " << b << "\"" << endl << endl;
+				}
+
+			}
+			
+
+
+		}
+
+		void findRes_A(Reservation a, string b)
+		{
+			vector <Reservation> vectemp;
+			int initsize = vectemp.size();
+			cout << "Find Results:" << endl;
+
+			if (a.getSailorInfo().getSID() == 0) {
+				for (int i = 0; i < reservations.size(); i++){
+					if (reservations[i].getBoatInfo().getBID() == a.getBoatInfo().getBID() && reservations[i].getDate() == a.getDate()) {
+						vectemp.push_back(reservations[i]);
+					}
+				}
+
+				if (vectemp.size() == initsize) { cout << "Error: No matching reservation found for boat id " << a.getBoatInfo().getBID() << " on " << b << endl << endl; }
+				else {
+					for (int i = 0; i < vectemp.size(); i++){
+						vectemp[i].printReservation();
+					}
+					cout << endl;
+				}
+
+			}
+
+			else if (a.getBoatInfo().getBID() == 0) {
+				for (int i = 0; i < reservations.size(); i++){
+					if (reservations[i].getSailorInfo().getSID() == a.getSailorInfo().getSID() && reservations[i].getDate() == a.getDate()) {
+						vectemp.push_back(reservations[i]);
+					}
+				}
+
+				if (vectemp.size() == initsize) { cout << "Error: No matching reservation found for sailor id " << a.getSailorInfo().getSID() << " on " << b << endl << endl; }
+				else {
+					for (int i = 0; i < vectemp.size(); i++){
+						vectemp[i].printReservation();
+					}
+					cout << endl;
+				}
+			}
+
+		}
+
+		void findRes_B(Date a, Date b, string c, string d)
+		{
+
+			vector <Reservation> vectemp;
+			int initsize = vectemp.size();
+			cout << "Find Results:" << endl;
+
+			for (int i = 0; i < reservations.size(); i++){
+				if(reservations[i].getDate() >= a && reservations[i].getDate() <= b) {
+					vectemp.push_back(reservations[i]);
+				}
+			}
+
+			if (vectemp.size() == initsize) { cout << "Error: No matching reservation found between dates " << c << " & " << d << endl << endl; }
+			else {
+				for (int i = 0; i < vectemp.size(); i++){
+					vectemp[i].printReservation();
+				}
+				cout << endl;
+			}
+
+		}
+
+		void sortAll()
+		{ 
+			int min,lenres = reservations.size();
+			Reservation temp;
+
+			for (int i = 0; i < lenres-1; i++) {
+
+			min = i;
+
+			for (int k = i + 1; k < lenres; k++) {
+
+				if (reservations[k].getDate() < reservations[min].getDate()) { min = k; }
+				else if (reservations[k].getDate() == reservations[min].getDate()) {
+					if (reservations[k].getSailorInfo().getSailorName() < reservations[min].getSailorInfo().getSailorName()) { min = k; }
+				}
+				
+			}
+						  
+			temp = reservations[i];         
+			reservations[i] = reservations[min];
+			reservations[min] = temp;
+
+			}
+			
+		}
 
 	private:
 		vector<Reservation> reservations;
